3c5ad96e2bb159501f4618277f901f2ab827da32
MBigaj <mikolaj.bigaj.00@gmail.com>
2025-12-01 00:08:41 +0100

    [refactor] - Updated all elements of poetry packages and npm, running Python 3.13 as main

--- backend/database/MongoDBSingleton.py
+++ backend/database/MongoDBSingleton.py
@@ -5,15 +5,15 @@ import os
 
 
 class MongoDBSingleton:
-    client: MongoClient
-    connection = None
+    _client: MongoClient
+    _connection = None
 
     def __init__(self):
         load_dotenv()
-        self.client = MongoClient(os.environ.get('DATABASE_CONNECTION_STRING'))
+        self._client = MongoClient(os.environ.get('DATABASE_CONNECTION_STRING'))
 
     def get_connection(self):
-        if not self.connection:
-            self.connection = self.client[os.environ.get('DATABASE_CLIENT')]
-        return self.connection
+        if not self._connection:
+            self._connection = self._client[os.environ.get('DATABASE_CLIENT')]
+        return self._connection
     
